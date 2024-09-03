87e6e0f791586cad19512943d89373093f01bab8
SushenShekar <sushenshekar@gmail.com>
2024-09-03 16:56:27 +0530

    First Upload

--- app/js/script.js
+++ app/js/script.js
@@ -4,9 +4,43 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has_fade");
 
-btnHamburger.addEventListener("click", () => {
+// btnHamburger.addEventListener("click", () => {
+//   if (header.classList.contains("open")) {
+//     //Close Hamburger Menu
+//     body.classList.remove("noscroll");
+//     header.classList.remove("open");
+//     fadeElems.forEach(function (element) {
+//       element.classList.remove("fade-in");
+//       element.classList.add("fade-out");
+//     });
+//   } else {
+//     //Open Hamburger Menu
+//     body.classList.add("noscroll");
+//     header.classList.add("open");
+//     fadeElems.forEach(function (element) {
+//       element.classList.remove("fade-out");
+//       element.classList.add("fade-in");
+//     });
+//   }
+// });
+
+btnHamburger.addEventListener("click", (event) => {
+  event.stopPropagation(); // Prevent the click event from propagating to the document
+  toggleMenu();
+});
+
+document.addEventListener("click", (event) => {
   if (header.classList.contains("open")) {
-    //Close Hamburger Menu
+    // Close the menu if a click is detected outside the header
+    if (!header.contains(event.target)) {
+      toggleMenu();
+    }
+  }
+});
+
+function toggleMenu() {
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
     body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElems.forEach(function (element) {
@@ -14,7 +48,7 @@ btnHamburger.addEventListener("click", () => {
       element.classList.add("fade-out");
     });
   } else {
-    //Open Hamburger Menu
+    // Open Hamburger Menu
     body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
@@ -22,4 +56,4 @@ btnHamburger.addEventListener("click", () => {
       element.classList.add("fade-in");
     });
   }
-});
+}
